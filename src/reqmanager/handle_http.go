419afd98fbce6bb28dc9217e23479f858e591739
Dmitry Korablin <korablindm@gmail.com>
2024-12-19 23:10:03 +0300

    Added back button to examine pages

--- src/reqmanager/handle_http.go
+++ src/reqmanager/handle_http.go
@@ -30,18 +30,24 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 type examineView struct {
-	Title string
-	Num   string
-	Text  string
+	Title    string
+	Num      string
+	Text     string
+	BackPage string
 }
 
 func examineHandler(w http.ResponseWriter, r *http.Request) {
 	num, err := strconv.Atoi(r.URL.Query().Get("num"))
-	// Wrong page
+	// Wrong num
 	if err != nil {
 		tmpl, _ := template.ParseFiles("static/templates/error.html")
 		tmpl.Execute(w, err)
 		return
+	}
+	backpage := r.URL.Query().Get("backpage")
+	// Wrong page
+	if backpage == "" {
+		backpage = "1"
 	}
 	data, err := dbmanager.GetElement(uint(num))
 	if err != nil {
@@ -49,7 +55,7 @@ func examineHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, err)
 		return
 	}
-	view := examineView { Num: strconv.Itoa(num + 1), Title: data.Title, Text: data.Text }
+	view := examineView { Num: strconv.Itoa(num + 1), Title: data.Title, Text: data.Text, BackPage: backpage }
 	tmpl, _ := template.ParseFiles("static/templates/examine.html")
 	tmpl.Execute(w, view)
 }
